4d6ad7f372654d8dc57c798fec72c219d8201106
Damian Matysko <damian.matysko@akademiasovy.sk>
2019-10-15 12:09:31 +0200

    ASC

---
+++ Fragment.java
@@ -0,0 +1,80 @@
+package sk.itsovy.matysko.projectfragment;
+
+public class Fragment {
+    private int numerator;
+    private int denominator;
+    //generate
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public Fragment(int numerator, int denominator) {
+        if (denominator==0)
+            denominator=1;
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+    public Fragment(Fragment other){//kopirovanie cez konstruktor
+        numerator=other.getNumerator();
+        denominator=other.getDenominator();
+    }
+
+
+    public void changeToBasicShape(){
+        int commnDividisor=getTheLargestCommonDivsor();
+
+    }
+    public void extendFunction(){
+
+    }
+    public double getRulValue(){
+        return 0;//todo
+    }
+    public void reverse(){
+        int temp=numerator;
+        numerator=denominator;
+        denominator=temp;
+    }
+    public void oposite(){
+
+    }
+    public Fragment copy(){
+        Fragment newFragment=new Fragment(numerator, denominator);
+        return newFragment;
+    }
+    private int getTheLargestCommonDivsor(){
+        int absNummerator=Math.abs(numerator);
+        int absDenominator=Math.abs(denominator);
+
+        int commonDividor=numerator<denominator?numerator<denominator;
+        while (absNummerator%commonDividor !=0|| absDenominator%commonDividor!=0){
+            commonDividor--;
+        }
+        return commonDividor;
+    }
+    public boolean isFragmentInBasicShape(){
+        if(getTheLargestCommonDivsor()==1){
+            return true;
+        }else{
+            return false;
+        }
+
+        @Override
+        public String toString(){
+            return numerator +"/" +denominator;
+        }
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,23 @@
+package sk.itsovy.matysko.projectfragment;
+
+public class Main {
+    public static void main (String[] args){
+
+        Fragment f1=new Fragment(32,48);
+        System.out.println(f1.toString());
+        f1.changeToBasicShape();;
+        System.out.println(f1.toString());
+        f1.extendFunction(-5);
+        System.out.println(f1.toString());
+        System.out.println(f1.getRulValue());
+
+        Fragment f2=new Fragment(7,28);
+        f2.changeToBasicShape();
+        System.out.println(f2.toString());
+        System.out.println(f2.isFragmentInBasicShape());
+
+        Fragment f3=new Fragment(7,0);
+        Fragment f4=f2.copy();
+
+    }
+}
